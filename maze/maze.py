c1736617eb99d35706edeb48e50554bf7c9448cb
mulsman08 <113217821+mulsman08@users.noreply.github.com>
2022-11-04 11:43:41 -0500

    Update maze.py
    EL BORRADOR DEL BORRADORR!!!!!!!!!!!!!!!!!!!!

--- maze/maze.py
+++ maze/maze.py
@@ -1,1 +1,17 @@
-
+import pygame
+import turtle
+v = turtle.Screen()
+d = turtle.Turtle()
+class Maze:
+    def __init__(self,personaje,muro,color):
+        self.p = personaje
+        self.m = muro
+        self.color = "negro"
+        self.ventana = ("500x500")
+    def mensaje (self):
+        msn = "Tendrás que cruzarel laberinto... ¡buena suerte!"
+        return msn
+    def crear_color(self):
+        self.color = "negro"
+        self. 
+    
