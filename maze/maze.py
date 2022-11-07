28e62c8df2dd0a185e852eb5975849148489cc52
mulsman08 <113217821+mulsman08@users.noreply.github.com>
2022-11-07 17:25:51 -0500

    Update maze.py
    N/A

--- maze/maze.py
+++ maze/maze.py
@@ -1,17 +1,73 @@
 import pygame
-import turtle
-v = turtle.Screen()
-d = turtle.Turtle()
-class Maze:
-    def __init__(self,personaje,muro,color):
-        self.p = personaje
-        self.m = muro
-        self.color = "negro"
-        self.ventana = ("500x500")
-    def mensaje (self):
-        msn = "Tendrás que cruzarel laberinto... ¡buena suerte!"
-        return msn
-    def crear_color(self):
-        self.color = "negro"
-        self. 
-    
+pygame.init()
+class Pared(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("")
+ancho =1200
+alto = 700
+movil = pygame.Rect(600,400,40,40)
+x = 0
+y=0
+vel = 0
+alt = 0
+negro (0,0,0)
+ventana = pygame.display.set_mode((ancho,alto))
+pygame.display.set_caption("muro")
+reloj = pygame.time.Clock()
+mapa =[
+    "XXXXXXXXXXX",
+    "X         X",
+    "X XXXXXXX X",
+    "X XXXXXXXXX",
+    "X    X    X",
+    "XX XX XX XX",
+    "XXXXXXXXXXX",
+
+]
+gameOver = False
+while not gameOver:
+    reloj.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameOver = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                vel =-5
+            elif event.key == pygame.K_RIGHT:
+                vel =+5
+            elif event.key == pygame.K_UP:
+                alt =-5
+            elif event.key == pygame.K_DOWN:
+                alt =+5
+            else:
+                vel = 0
+                alt = 0
+            movil.x += vel
+            movil.y += alt
+
+            bola.rect.x = movil.x
+            bola.rect.y = movil.y
+
+            for muro in listaMuro :
+                if movil.colliderect(muro):
+                    movil.x -= vel
+                    movil.y -= alt
+            ##################################
+            ventana.fill(NEGRO)
+            ##################################
+            x = 0
+            y = 0
+            for fila in mapa:
+                for muro in fila:
+                    if muro == "X":
+                        pared.rect.x=x
+                        pared.rect.y=y
+                        listaPared.add(Pared)
+                        listaPared.draw(ventana)
+                    x += 80
+                x =0
+                y +=80
+            listaBola.draw(ventana)
+            pygame.display.flip()
+        pygame.quit()
