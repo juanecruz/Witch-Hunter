e05a954735ddbdd95762547895bd40e986243a77
mulsman08 <113217821+mulsman08@users.noreply.github.com>
2022-11-11 12:05:21 -0500

    Update maze.py
    ...

--- maze/maze.py
+++ maze/maze.py
@@ -1,9 +1,24 @@
 import pygame
 pygame.init()
-class Pared(pygame.sprite.Sprite):
+class Bola (pygame.sprite.Sprite): # aqui se 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("")
+        self.image = pygame.image.load("E: /").convert_alpha()
+        self.rect = self.image.get_rect()
+#-------------------------------------- MUROS------------------------------------------------
+def construir_mapa (mapa):
+    listaMuros = []
+    x = 0
+    y = 0
+    for fila in mapa:
+        for muro in fila:
+            listaMuros.append(pygame.Rect(x,y,80,80))
+            x
+class Pared (pygame.sprite.Sprite): # aqui se carga una imagencon sprite
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("E: /").convert_alpha()
+        self.rect = self.image.get_rect()
 ancho =1200
 alto = 700
 movil = pygame.Rect(600,400,40,40)
@@ -11,7 +26,7 @@ x = 0
 y=0
 vel = 0
 alt = 0
-negro (0,0,0)
+negro  = (0,0,0)
 ventana = pygame.display.set_mode((ancho,alto))
 pygame.display.set_caption("muro")
 reloj = pygame.time.Clock()
@@ -46,8 +61,8 @@ while not gameOver:
             movil.x += vel
             movil.y += alt
 
-            bola.rect.x = movil.x
-            bola.rect.y = movil.y
+            Bola.rect.x = movil.x
+            Bola.rect.y = movil.y
 
             for muro in listaMuro :
                 if movil.colliderect(muro):
@@ -61,13 +76,13 @@ while not gameOver:
             for fila in mapa:
                 for muro in fila:
                     if muro == "X":
-                        pared.rect.x=x
-                        pared.rect.y=y
+                        Pared.rect.x=x
+                        Pared.rect.y=y
                         listaPared.add(Pared)
                         listaPared.draw(ventana)
                     x += 80
                 x =0
                 y +=80
-            listaBola.draw(ventana)
+            lista.Bola.draw(ventana)
             pygame.display.flip()
         pygame.quit()
